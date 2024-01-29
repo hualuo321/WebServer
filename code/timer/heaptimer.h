9ffb18c11d3644bf6802bc9c96325e6d5c1fa25c
hualuo321 <15707220966@163.com>
2024-01-29 20:39:04 +0800

    Update heaptimer.h

--- code/timer/heaptimer.h
+++ code/timer/heaptimer.h
@@ -1,8 +1,3 @@
-/*
- * @Author       : mark
- * @Date         : 2020-06-17
- * @copyleft Apache 2.0
- */ 
 #ifndef HEAP_TIMER_H
 #define HEAP_TIMER_H
 
@@ -16,38 +11,36 @@
 #include <chrono>
 #include "../log/log.h"
 
-typedef std::function<void()> TimeoutCallBack;
-typedef std::chrono::high_resolution_clock Clock;
-typedef std::chrono::milliseconds MS;
-typedef Clock::time_point TimeStamp;
+typedef std::function<void()> TimeoutCallBack;      // 定义回调函数类型
+typedef std::chrono::high_resolution_clock Clock;   // 使用高精度时钟
+typedef std::chrono::milliseconds MS;               // 毫秒单位
+typedef Clock::time_point TimeStamp;                // 时间点类型
 
+// 定时器节点结构
 struct TimerNode {
-    int id;
-    TimeStamp expires;
-    TimeoutCallBack cb;
-    bool operator<(const TimerNode& t) {
+    int id;                                         // 定时器标识
+    TimeStamp expires;                              // 到期时间
+    TimeoutCallBack cb;                             // 到期回调
+    bool operator<(const TimerNode& t) {            // 比较运算符，用于确定优先级
         return expires < t.expires;
     }
 };
+
+// 堆定时器类
 class HeapTimer {
 public:
-    HeapTimer() { heap_.reserve(64); }
-
-    ~HeapTimer() { clear(); }
+    HeapTimer() { heap_.reserve(64); }              // 构造函数
+    ~HeapTimer() { clear(); }                       // 析构函数
     
-    void adjust(int id, int newExpires);
-
+    void adjust(int id, int newExpires);            // 调整定时器
+    // 添加定时器
     void add(int id, int timeOut, const TimeoutCallBack& cb);
-  
-    void siftup_(size_t i);
-
-    bool siftdown_(size_t index, size_t n);
-
-    void SwapNode_(size_t i, size_t j);
-
-    std::vector<TimerNode> heap_;
-
-    std::unordered_map<int, size_t> ref_;
+    void siftup_(size_t i);                         // 向上调整堆
+    bool siftdown_(size_t index, size_t n);         // 向下调整堆
+    void SwapNode_(size_t i, size_t j);             // 交换节点
+    
+    std::vector<TimerNode> heap_;                   // 定时器堆
+    std::unordered_map<int, size_t> ref_;           // 定时器引用，用于快速定位
 };
 
 #endif //HEAP_TIMER_H
