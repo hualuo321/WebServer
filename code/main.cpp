daf94af60f4174d93dd24842dc2357b58980b040
hualuo321 <15707220966@163.com>
2024-01-29 16:51:10 +0800

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,18 +1,12 @@
-/*
- * @Author       : mark
- * @Date         : 2020-06-18
- * @copyleft Apache 2.0
- */ 
 #include <unistd.h>
 #include "server/webserver.h"
 
 int main() {
-    //daemon(1, 0); 
+    // 守护进程， 后台运行
+    // daemon(1, 0); 
 
-    WebServer server(
-        1316, 3, 60000, false, 
-        3306, "root", "root", "webserver", 
-        12, 6, true, 1, 1024);             
+    // 服务器初始化 (port, ET, timeout, 优雅退出, sqlPort, user, passwd, sqlName, connNum, threadNum, 日志, 日志等级, 日志队列大小)
+    WebServer server(1316, 3, 60000, false, 3306, "root", "root", "webserver", 12, 6, true, 1, 1024);             
     server.Start();
 } 
   
