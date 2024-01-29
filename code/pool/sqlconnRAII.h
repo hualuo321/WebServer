05e75816c0b442014b46440dba2396c745bf9204
hualuo321 <15707220966@163.com>
2024-01-29 16:46:05 +0800

    Update sqlconnRAII.h

--- code/pool/sqlconnRAII.h
+++ code/pool/sqlconnRAII.h
@@ -1,30 +1,25 @@
-/*
- * @Author       : mark
- * @Date         : 2020-06-19
- * @copyleft Apache 2.0
- */ 
-
 #ifndef SQLCONNRAII_H
 #define SQLCONNRAII_H
 #include "sqlconnpool.h"
 
-/* 资源在对象构造初始化 资源在对象析构时释放*/
+// SqlConnRAII 类用于自动管理 SQL 连接的获取与释放
 class SqlConnRAII {
 public:
+    // 构造函数
     SqlConnRAII(MYSQL** sql, SqlConnPool *connpool) {
-        assert(connpool);
-        *sql = connpool->GetConn();
-        sql_ = *sql;
-        connpool_ = connpool;
+        assert(connpool);                       // 断言连接池不为空
+        *sql = connpool->GetConn();             // 从连接池获取一个数据库连接
+        sql_ = *sql;                            // 保存获取的连接
+        connpool_ = connpool;                   // 保存连接池的引用
     }
-    
+    // 析构函数
     ~SqlConnRAII() {
-        if(sql_) { connpool_->FreeConn(sql_); }
+        if(sql_) { connpool_->FreeConn(sql_); } // 释放连接
     }
     
 private:
-    MYSQL *sql_;
-    SqlConnPool* connpool_;
+    MYSQL *sql_;                                // SQL连接指针
+    SqlConnPool* connpool_;                     // 连接池对象的指针
 };
 
 #endif //SQLCONNRAII_H
