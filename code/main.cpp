dd4922c39bdc45300e46dc0dea1b43e27b3aef88
hualuo321 <15707220966@163.com>
2024-01-30 13:29:20 +0800

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -5,7 +5,7 @@ int main() {
     // 当控制终端被关闭时，相应的进程都会自动关闭。若不想让进程因终端的关闭而受影响，需要把进程变为守护进程。
     daemon(1, 0); 
 
-    // 服务器初始化 (port, ET, timeout, optQuit, sqlPort, user, passwd, sqlName, sqlNum, threadNum, Log, LogLevel, LogQueSize)
+    // 服务器初始化 (port, ET, timeout, optLinger, sqlPort, user, passwd, sqlName, sqlNum, threadNum, Log, LogLevel, LogQueSize)
     WebServer server(1316, 3, 60000, false, 3306, "root", "root", "webserver", 12, 6, true, 1, 1024);     
 
     // 服务器启动        
